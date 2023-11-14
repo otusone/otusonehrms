1dc67542b2d7c501b744b71ce7117c0ef5cc817b
anujkumarchoudhary <anujkumarchoudhary6699@gmail.com>
2023-11-14 03:31:46 -0800

    new hrms

--- screen/src/components/sidebar/menuData.tsx
+++ screen/src/components/sidebar/menuData.tsx
@@ -4,47 +4,22 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { PiNote } from 'react-icons/pi';
 import { AiOutlineClockCircle } from 'react-icons/ai';
 import { FaGraduationCap } from 'react-icons/fa';
+import { GrDocumentTime } from "react-icons/gr";
+import { TbCalendarTime } from "react-icons/tb";
+import { FaTasks } from "react-icons/fa";
 
 export const menuData = [
     {
         "id": 1,
         "icon": <AiOutlineHome />,
         "title": "Dashboard",
-        "link": "/",
-        "subtitle": [
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "Overview",
-                "subLink": "#!"
-            },
-            {
-                "id": 2,
-                "icon": <AiOutlineHome />,
-                "menu": "Report",
-                "subLink": "/report"
-            }
-        ]
+        "link": "/"
     },
     {
         "id": 2,
         "icon": <AiOutlineTeam />,
         "title": "Staff",
-        "link": "/user",
-        "subtitle": [
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "User",
-                "subLink": "/user"
-            },
-            {
-                "id": 3,
-                "icon": <AiOutlineHome />,
-                "menu": "Employee Profile",
-                "subLink": "/employee-profile"
-            }
-        ]
+        "link": "/user"
     },
     {
         "id": 3,
@@ -57,52 +32,29 @@ export const menuData = [
         "icon": <PiNote />,
         "title": "Payroll",
         "link": "/payroll",
-        "subtitle": [
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "Set Salary",
-                "subLink": "/set-salary"
-            },
-            {
-                "id": 2,
-                "icon": <AiOutlineHome />,
-                "menu": "Payslip",
-                "subLink": "/payslip"
-            }
-        ]
     },
     {
         "id": 5,
         "icon": <AiOutlineClockCircle />,
         "title": "Timesheet",
         "link": "/timesheet",
-        "subtitle": [
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "Timesheet",
-                "subLink": "/timesheet"
-            },
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "Manage Leave",
-                "subLink": "/manage-leave"
-            },
-            {
-                "id": 1,
-                "icon": <AiOutlineHome />,
-                "menu": "Attandance",
-                "subLink": "/attandance"
-            }
-        ]
     },
     {
         "id": 6,
-        "icon": <AiOutlineClockCircle />,
+        "icon": <GrDocumentTime/>,
+        "title": "Manage Leave",
+        "link": "/manage-leave",
+    },
+    {
+        "id": 7,
+        "icon": <TbCalendarTime />,
+        "title": "Attandance",
+        "link": "/attandance",
+    },
+    {
+        "id": 8,
+        "icon": <FaTasks />,
         "title": "Ticket",
         "link": "/ticket",
-        "subtitle": []
     }
 ]
