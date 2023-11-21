2e2093c667ea158687c155f6a81651e5c33b200a
anujkumarchoudhary <anujkumarchoudhary6699@gmail.com>
2023-11-21 08:08:41 -0800

    new hrms

--- screen/src/components/tableData/employeeTable/EmployeeTable.tsx
+++ screen/src/components/tableData/employeeTable/EmployeeTable.tsx
@@ -6,7 +6,6 @@ import { RiDeleteBinLine } from 'react-icons/ri';
 import { BiRightArrow } from 'react-icons/bi';
 import CommonButton from '../../common/CommonButton/CommonButton';
 import SearchBox from '../../common/searchBox/SearchBox';
-import axios from 'axios';
 
 export interface IEmployeeTable {
     heading: string;
@@ -15,15 +14,21 @@ export interface IEmployeeTable {
     handleLeaveAction: any;
     handleEdit: any;
     handleDelete: any;
+    setQuery: any;
+    query: any;
 }
 
-const EmployeeTable = ({ heading, tableTitle, tableData, handleLeaveAction, handleEdit, handleDelete }: IEmployeeTable) => {
+const EmployeeTable = ({ heading, query, setQuery, tableTitle, tableData, handleLeaveAction, handleEdit, handleDelete }: IEmployeeTable) => {
 
     return (
         <Grid className={styles.commonTableContainer}>
             <TableHead className={styles.tableHead}>
                 <TableCell sx={{ fontSize: 20 }}>{heading}</TableCell>
-                <TableCell sx={{ fontSize: 20 }}><SearchBox /></TableCell>
+                <TableCell sx={{ fontSize: 20 }}>
+                    <SearchBox
+                        setQuery={setQuery}
+                    />
+                </TableCell>
             </TableHead>
             <TableContainer>
                 <Table>
@@ -37,28 +42,32 @@ const EmployeeTable = ({ heading, tableTitle, tableData, handleLeaveAction, hand
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {tableData.map((item: any) => {
-                            return (
-                                <TableRow key={item.id}>
-                                    <TableCell>
-                                        <CommonButton name={item.emp_id} onClick={(() => console.log("hi"))} />
-                                    </TableCell>
-                                    <TableCell>{item.name}</TableCell>
-                                    <TableCell>{item.email}</TableCell>
-                                    <TableCell>{item.branch}</TableCell>
-                                    <TableCell>{item.department}</TableCell>
-                                    <TableCell>{item.designation}</TableCell>
-                                    <TableCell>{item.dateOfJoin}</TableCell>
-                                    <TableCell className={styles.tableAction}>
-                                        <BiRightArrow onClick={handleLeaveAction} fontSize={30} />
+                        {
+                            tableData.filter((employee: { name: string; }) => {
+                                return query === '' || (employee.name?.toLowerCase()?.includes(query.toLowerCase()) ?? false);
+                            }).map((item: any) => {
+                                return (
+                                    <TableRow key={item.id}>
+                                        <TableCell>
+                                            <CommonButton name={item.emp_id} onClick={(() => console.log("hi"))} />
+                                        </TableCell>
+                                        <TableCell>{item.name}</TableCell>
+                                        <TableCell>{item.email}</TableCell>
+                                        <TableCell>{item.branch}</TableCell>
+                                        <TableCell>{item.department}</TableCell>
+                                        <TableCell>{item.designation}</TableCell>
+                                        <TableCell>{item.dateOfJoin}</TableCell>
+                                        <TableCell className={styles.tableAction}>
+                                            <BiRightArrow onClick={handleLeaveAction} fontSize={30} />
 
-                                        <MdOutlineMode onClick={handleEdit} fontSize={30}
-                                        />
-                                        <RiDeleteBinLine onClick={handleDelete} fontSize={30} />
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })}
+                                            <MdOutlineMode onClick={handleEdit} fontSize={30}
+                                            />
+                                            <RiDeleteBinLine onClick={handleDelete} fontSize={30} />
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            })
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
