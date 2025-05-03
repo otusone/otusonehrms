81d9f1678a30d121666a251a41e4b9ff1b4b3a82
SimranOTUSONE <simranotusonellp@gmail.com>
2025-05-03 17:02:41 +0530

    attendance list

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -1,11 +1,12 @@
 const mongoose=require("mongoose");
 const attendanceSchema=new mongoose.Schema({
     userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-    clockIn:{type:Date,required:true},
-    clockOut:{type:Date,required:true},
+    clockIn:{type:Date,required:false},
+    clockOut:{type:Date,required:false},
     date:{type:String,required:true},
     clockInLocation: {latitude: { type: Number, required: true },longitude: { type: Number, required: true },},
     clockOutLocation: {latitude: { type: Number },longitude: { type: Number },},
+    workingHours:{type:Number,default:0},
  
 },{ timestamp:true})
 
--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 // const {Auth,isUser,}=require("../middleware/auth")
 const {register,login} = require("../controllers/Auth/userAuthCtrl");
 const userController = require("../controllers/Auth/userAuthCtrl");
-const {markAttendance}= require("../controllers/attendanceCtrl");
+const {markClockIn,markClockOut,getAtendanceByDate,getMonthlyReport}= require("../controllers/attendanceCtrl");
 const { userAuth } = require("../middleware/auth");
 const router=express.Router();
 
@@ -14,8 +14,10 @@ router.post("/login", login);
 router.get('/profile',userAuth, userController.getProfile);
 router.patch("/updateprofile",userAuth,userController.updateProfile);
 router.delete("/deleteprofile",userAuth,userController.deleteProfile);
-router.post("/clockinattendance",userAuth,markAttendance);
-router.patch("/clockoutattendance",userAuth,markAttendance);
+router.post("/clockinattendance",userAuth,markClockIn);
+router.patch("/clockoutattendance/:id",userAuth,markClockOut);
+router.get("/attendance-list",userAuth,getAtendanceByDate);
+router.get("/attendance-report",userAuth,getMonthlyReport);
 
 // router.get('/profile', userAuthCtrl.getProfile);
 
