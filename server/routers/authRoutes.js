08b118ddf614105223b8f19e6d056b2115eb9399
ishita-072002 <ishita.otusone@gmail.com>
2025-05-06 12:02:47 +0530

    updated salary slip auth part and fixed some errors

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -1,14 +1,17 @@
-const express=require("express");
+const express = require("express");
 // const {Auth,isUser,}=require("../middleware/auth")
-const {register,login} = require("../controllers/Auth/authController");
+const { register, login } = require("../controllers/Auth/authController");
+const { adminAuth } = require("../middleware/adminAuth");
+const { generateSalarySlip, deleteSalarySlip, updateSalarySlip } = require("../controllers/SalaryCtrl");
 
-
-const router=express.Router();
+const router = express.Router();
 
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/salary-slip", adminAuth, generateSalarySlip);
+router.patch("/salary-slip/:id", adminAuth, updateSalarySlip);
+router.delete("/delete-slip/:id", adminAuth, deleteSalarySlip);
 
 
-
-module.exports=router;
+module.exports = router;
--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -1,34 +1,34 @@
-const express=require("express");
+const express = require("express");
 // const {Auth,isUser,}=require("../middleware/auth")
-const {register,login} = require("../controllers/Auth/userAuthCtrl");
+const { register, login } = require("../controllers/Auth/userAuthCtrl");
 const userController = require("../controllers/Auth/userAuthCtrl");
-const {markClockIn,markClockOut,getAtendanceByDate,getMonthlyReport}= require("../controllers/attendanceCtrl");
+const { markClockIn, markClockOut, getAtendanceByDate, getMonthlyReport } = require("../controllers/attendanceCtrl");
 const { userAuth } = require("../middleware/auth");
-const{applyForLeave,updateLeave,deleteLeave}=require("../controllers/leaveCtrl");
-const{generateSalarySlip,deleteSalarySlip,updateSalarySlip}=require("../controllers/SalaryCtrl");
-const router=express.Router();
+const { applyForLeave, updateLeave, deleteLeave } = require("../controllers/leaveCtrl");
+const { generateSalarySlip, deleteSalarySlip, updateSalarySlip } = require("../controllers/SalaryCtrl");
+const router = express.Router();
 
 router.use(express.json());
 
 
 router.post("/register", register);
 router.post("/login", login);
-router.get('/profile',userAuth, userController.getProfile);
-router.patch("/updateprofile",userAuth,userController.updateProfile);
-router.delete("/deleteprofile",userAuth,userController.deleteProfile);
-router.post("/clockinattendance",userAuth,markClockIn);
-router.patch("/clockoutattendance/:id",userAuth,markClockOut);
-router.get("/attendance-list",userAuth,getAtendanceByDate);
-router.get("/attendance-report",userAuth,getMonthlyReport);
-router.post("/leave",userAuth,applyForLeave);
-router.patch("/leave/:id",userAuth,updateLeave),
-router.delete("/delete-leave/:id")
-router.post("/salary-slip",userAuth,generateSalarySlip);
-router.patch("/salary-slip/:id",userAuth,updateSalarySlip);
-router.delete("/delete-slip/:id"),userAuth,deleteSalarySlip;
+router.get('/profile', userAuth, userController.getProfile);
+router.patch("/updateprofile", userAuth, userController.updateProfile);
+router.delete("/deleteprofile", userAuth, userController.deleteProfile);
+router.post("/clockinattendance", userAuth, markClockIn);
+router.patch("/clockoutattendance/:id", userAuth, markClockOut);
+router.get("/attendance-list", userAuth, getAtendanceByDate);
+router.get("/attendance-report", userAuth, getMonthlyReport);
+router.post("/leave", userAuth, applyForLeave);
+router.patch("/leave/:id", userAuth, updateLeave);
+router.delete("/delete-leave/:id", userAuth, deleteLeave);
+// router.post("/salary-slip", userAuth, generateSalarySlip);
+// router.patch("/salary-slip/:id", userAuth, updateSalarySlip);
+// router.delete("/delete-slip/:id", userAuth, deleteSalarySlip);
 
 // router.get('/profile', userAuthCtrl.getProfile);
 
 
 
-module.exports=router;
+module.exports = router;
