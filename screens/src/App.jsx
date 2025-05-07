47aa30bfa2dc8134c496df31c536d7c7c5ccd95b
SimranOTUSONE <simranotusonellp@gmail.com>
2025-05-07 11:54:28 +0530

    dashboard

--- screens/src/App.jsx
+++ screens/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Login from "./components/Login";
 import { Routes, Route } from 'react-router-dom'; 
+import Dashboard from "./components/Dashboard";
 
 function App(){
   return(
     <div>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
     </div>
   )
