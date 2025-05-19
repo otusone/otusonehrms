50e45e11c493839654abf12c83caa39702fc19ef
ishita-072002 <ishita.otusone@gmail.com>
2025-05-19 17:17:00 +0530

    baseURL changes

--- screens/src/utils/baseurl.js
+++ screens/src/utils/baseurl.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+const baseURL = "https://otusonehrms-1utc.onrender.com/api/v1"
+//const baseURL = 'http://localhost:8000/api/v1'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: baseURL,
 });
 
 export default axiosInstance;
