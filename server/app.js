51bbc0779b1815ce1f96fb3233c4cc127755360c
ishita-072002 <ishita.otusone@gmail.com>
2025-05-19 16:47:52 +0530

    server changes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const routes = require("./routers/index");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-const buildPath = path.join(__dirname, './Dashboard', 'dist');
 const path = require('path');
 
 
@@ -31,16 +30,16 @@ app.use((err, req, res, next) => {
 });
 
 
-app.use((req, res) => {
-    res.status(404).json({ message: "Route not found!" });
-});
 
 
-//const buildPath = path.join(__dirname, './Dashboard','dist')
-
+const buildPath = path.join(__dirname, '../screens', 'dist')
 app.use(express.static(buildPath))
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Dashboard', 'dist', "index.html"));
+    res.sendFile(path.join(__dirname, '../screens', 'dist', "index.html"));
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found!" });
 });
 
 app.listen(PORT, () => {
